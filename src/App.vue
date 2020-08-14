881fa3f443a044b6f045f7d6dafbb380e27cdd95
Martin.Q <martinq@MartinQ-MacBookPro.local>
2020-08-15 00:45:13 +0900

    add xxx page

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
   update title
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/xxx">xxx</router-link>
     </div>
     <router-view/>
   </div>
