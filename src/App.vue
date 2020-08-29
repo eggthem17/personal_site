c0319cf80cbbbc9bcdced166b3467773c63be411
paxmiler <paxmiler@gmail.com>
2020-08-29 20:34:00 +0900

    add menu list/connect firebase/test realtime base

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
     <v-app-bar-nav-icon app @click="drawer = !drawer"/>
     <site-title :title="title"></site-title>
     <v-spacer/>
+    <v-btn icon @click='save'><v-icon>mdi-check</v-icon></v-btn>
+    <v-btn icon @click='read'><v-icon>mdi-numeric</v-icon></v-btn>
+    <v-btn icon @click='readOne'><v-icon>mdi-heart</v-icon></v-btn>
     <v-btn icon to="/about">
         <v-icon>mdi-magnify</v-icon>
     </v-btn>
@@ -43,6 +46,27 @@ export default {
     return {
       drawer: false,
       title: "걔랑계란's Chillog"
+    }
+  },
+  mounted () {
+    console.log(this.$firebase)
+  },
+  methods: {
+    save () {
+      console.log('save@@@')
+      this.$firebase.database().ref().child('abcd').set({
+        title: 'abcd', text: 'tttttttttt'
+      })
+    },
+    read () {
+      this.$firebase.database().ref().child('abcd').on('value', (sn) => {
+        console.log(sn)
+        console.log(sn.val())
+      })
+    },
+    async readOne () {
+      const sn = await this.$firebase.database().ref().child('abcd').once('value')
+      console.log(sn.val())
     }
   }
 }
