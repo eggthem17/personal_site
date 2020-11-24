951d1c1cabab6c7c8ffed0cce2075363ce0e6611
paxmiler <paxmiler@gmail.com>
2020-11-24 18:04:51 +0900

    code match

--- src/components/display-user.vue
+++ src/components/display-user.vue
@@ -1,16 +1,15 @@
 <template>
-    <v-tooltip bottom>
-        <template v-slot:activator="{on}">
-          <!--  <span v-on="on">{{user.displayName}}</span>-->
-          <v-chip v-on="on" color="accent">
-            <v-avatar left>
-            <v-img :src="user.photoURL"/>
-          </v-avatar>
-          {{user.displayName}}
-          </v-chip>
-        </template>
-        <span>{{user.email}}</span>
-    </v-tooltip>
+  <v-tooltip bottom>
+    <template v-slot:activator="{on}">
+      <v-chip v-on="on" color="accent">
+        <v-avatar left>
+          <v-img :src="user.photoURL"/>
+        </v-avatar>
+        <span class="text-truncate">{{user.displayName}}</span>
+      </v-chip>
+    </template>
+    <span>{{user.email}}</span>
+  </v-tooltip>
 </template>
 <script>
 export default {
